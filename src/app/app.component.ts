3f4598b1eeaefb7e7b398d5ebde202b2d33e6424
Danuka <danukakoshitha2004@gmail.com>
2025-03-21 18:50:48 +0530

    create the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { MainDashboardComponent } from "./pages/main-dashboard/main-dashboard.component";
+import { PagesComponent } from "./pages/pages.component";
+import { RegisterFormComponent } from "./pages/register-form/register-form.component";
+import { LoginFormComponent } from "./pages/login-form/login-form.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [MainDashboardComponent, PagesComponent, RegisterFormComponent, LoginFormComponent,RouterOutlet]
 })
 export class AppComponent implements OnInit {
   title = 'web-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainDashboardComponent } from './pages/main-dashboard/main-dashboard.component';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:MainDashboardComponent
+    },
+    {
+        path:"register",
+        component:RegisterFormComponent
+    }
+];
